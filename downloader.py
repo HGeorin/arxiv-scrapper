57b4700c87c12f604de21605f8deb661c87b5ebc
HGeorin <982427105@qq.com>
2025-02-25 17:57:53 +0800

    设置下载单个文件的超时时间

--- downloader.py
+++ downloader.py
@@ -5,7 +5,7 @@ import time
 import logging
 from pymongo import MongoClient
 
-logging.basicConfig(filename='./logs/downloader.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='./logs/cs-downloader.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # 连接 MongoDB
 client = MongoClient("mongodb://localhost:27017/")
@@ -20,6 +20,9 @@ BASE_DIR = "./paper_storage"
 
 print("downloading files. Press Ctrl + C to cancel ...")
 
+# 下载超时时间
+max_time = 10 * 60  # 最大下载时间
+
 for paper in papers:
     arxiv_id = paper["arxivId"]
     file_path = paper["file_path"]
@@ -33,21 +36,38 @@ for paper in papers:
     # 确保目录存在
     os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
+    # 检查文件是否存在，若存在则删除
+    if os.path.exists(full_path):
+        os.remove(full_path)
+
     try:
         # 下载 PDF
-        response = requests.get(pdf_url, stream=True)
+        response = requests.get(pdf_url, stream=True, timeout=60)
         if response.status_code == 200:
+            download_flag = 1
             with open(full_path, "wb") as pdf_file:
+                start_time = time.time()  # 记录开始时间
                 for chunk in response.iter_content(chunk_size=1024):
+                    # 检查下载总时长
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time > max_time:  # 如果下载时间超过最大限制
+                        os.remove(full_path)
+                        logging.warning(f"⚠️ download timeout and skip: {arxiv_id}")
+                        download_flag = 0
+                        break
                     pdf_file.write(chunk)
             
             # 下载成功，更新数据库
-            collection.update_one({"arxivId": arxiv_id}, {"$set": {"download_mark": 1}})
-            logging.info(f"✅ download successfully : {full_path}")
+            if download_flag:
+                collection.update_one({"arxivId": arxiv_id}, {"$set": {"download_mark": 1}})
+                logging.info(f"✅ download successfully : {full_path}")
         else:
-            logging.warning(f"❌ download failed: {arxiv_id}，status: {response.status_code}")
+            logging.warning(f"⚠️ download failed: {arxiv_id}，status: {response.status_code}")
+    except requests.exceptions.Timeout:
+        os.remove(full_path)
+        logging.warning(f"⚠️ connection timeout and skip: {arxiv_id}")
     except Exception as e:
-        logging.info(f"⚠️ Error: {arxiv_id}, {e}")
+        logging.error(f"❌ {arxiv_id}, {e}")
     time.sleep(10)
 
 logging.info("📂 all the papers have been downloaded ")
